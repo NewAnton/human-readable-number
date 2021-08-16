509cf730932ef8f9a54a220a415558551a6a7919
NewAnton <1anton1korobov@gmail.com>
2021-08-16 21:23:22 +0300

    feat: all done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+  let oneArray = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  let teenArray = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  let tenArray = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+  let answer = ''
+
+  if (number === 0) {
+    return 'zero'
+  }
+
+  huoneNumber(number)
+  number = number % 100
+  oneNumber(number)
+  teenNumber(number)
+  tenNumber(number)
+
+  function huoneNumber(number) {
+    if ((number >= 100) && (number <= 999)) {
+      answer = answer + oneArray[number.toString()[0]] + ' hundred '
+    }
+    return
+  }
+
+  function oneNumber(number) {
+    if (number <= 9) {
+      answer = answer + oneArray[number]
+    }
+    return
+  }
+
+  function teenNumber(number) {
+    if ((number >= 10) && (number <= 19)) {
+      answer = answer + teenArray[number.toString()[1]]
+    }
+    return
+  }
+
+  function tenNumber(number) {
+    if ((number >= 20) && (number <= 99)) {
+      answer = answer + tenArray[number.toString()[0]] + ' ' + oneArray[number.toString()[1]]
+    }
+    return
+  }
+
+  return answer.trim()
 }
